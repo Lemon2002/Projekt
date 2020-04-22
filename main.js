7da67448683f88d5ab82187958d57a6a52abbe1a
Lemon2002 <64126633+Lemon2002@users.noreply.github.com>
2020-04-22 10:21:44 +0200

    Add index.html style.css main.js programista.jpg zdjęcie.jpeg

---
+++ main.js
@@ -0,0 +1,9 @@
+var a = prompt("Podaj a");
+var b = prompt("Podaj b");
+var a1 = parseInt(a);
+var b1 = parseInt(b);
+
+var wynikpole = a1 * b1;
+var wynikobwod = 2 * a1 + 2 * b1;
+
+alert("Pole wynosi " + wynikpole + ", a obwód " + wynikobwod);
